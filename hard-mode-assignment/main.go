ad05ecf1f261f8888ed5d24b475bc7b6fa4dd665
Bharat Verma <bindian0509@gmail.com>
2021-05-23 15:31:33 +0530

    Interface simple assignment completed

---
+++ hard-mode-assignment/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	fmt.Println(os.Args)
+	fmt.Println(os.Args[1])
+}
--- http/main.go
+++ http/main.go
@@ -27,10 +27,40 @@ func main() {
 
 	lw := logWriter{}
 	io.Copy(lw, resp.Body)
+
+	sq := square{16}
+	tr := triangle{10, 20}
+
+	printArea(sq)
+	printArea(tr)
+
 }
 
 func (logWriter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
 	fmt.Println("Just wrote these many bytes : ", len(bs))
 	return len(bs), nil
+}
+
+/* part of the assignment on interface */
+type shape interface {
+	area() float64
+}
+
+type square struct {
+	sideLength float64
+}
+type triangle struct {
+	base   float64
+	height float64
+}
+
+func (s square) area() float64 {
+	return s.sideLength * s.sideLength
+}
+func (t triangle) area() float64 {
+	return (t.base * t.height) / 2
+}
+func printArea(s shape) {
+	fmt.Println(s.area())
 }
