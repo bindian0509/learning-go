cff41f8e8fa1ece0c2a85d63dae7a8ecd80207e7
Bharat Verma <bindian0509@gmail.com>
2021-05-23 14:46:52 +0530

    Done with interface

--- http/main.go
+++ http/main.go
@@ -1,11 +1,16 @@
 package main
 
+/* imports */
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
+type logWriter struct{}
+
+/* main method */
 func main() {
 
 	resp, err := http.Get("https://insurance.mobikwik.com/api/home/ping")
@@ -14,5 +19,18 @@ func main() {
 		fmt.Println("Error : ", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+	//fmt.Println(resp)
+
+	/* bs := make([]byte, 99999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs)) */
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote these many bytes : ", len(bs))
+	return len(bs), nil
 }
