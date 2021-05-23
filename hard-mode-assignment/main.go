b44709558261fe36e788f9e8b529a12a9c46a89d
Bharat Verma <bindian0509@gmail.com>
2021-05-23 16:37:27 +0530

    Hard Assignment for Interfaces completed

--- hard-mode-assignment/main.go
+++ hard-mode-assignment/main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 
-	fmt.Println(os.Args)
-	fmt.Println(os.Args[1])
+	//fmt.Println(os.Args)
+	//fileName := string(os.Args[1])
+	f, err := os.Open(os.Args[1])
+	//bs, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error : ", err)
+		os.Exit(1)
+	}
+	io.Copy(os.Stdout, f)
 }
+
+/*
+
+	usage : go run main.go myfile.txt
+	go build main.go <- in order to print contents of the main.go
+	./main main.go <- will print main.go
+*/
