da4a30e90179dee332f4b1dc1000376911c1cea1
Bharat Verma <bindian0509@gmail.com>
2022-06-21 01:00:49 +0530

    Web App HTML 5

--- web-app-html/cmd/web/main.go
+++ web-app-html/cmd/web/main.go
@@ -11,8 +11,8 @@ const portNum = ":8080"
 
 // main - open the link - http://localhost:8080/
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting web-app application the first go web application on port %s", portNum))
 	http.ListenAndServe(portNum, nil)
