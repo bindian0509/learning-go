382fef6d1522e77febc0f8962970d3f0cbb78bf7
Bharat V <bindian0509@gmail.com>
2023-08-27 00:36:15 +0530

    trying hands on with gin framework

--- linkedin/main.go
+++ linkedin/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "github.com/gin-gonic/gin"
 
 type Dimension struct {
 	length int
@@ -44,4 +45,13 @@ func main () {
 	fmt.Println(d)
 	fmt.Println(d.area())
 	fmt.Println(d)
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+
 }
